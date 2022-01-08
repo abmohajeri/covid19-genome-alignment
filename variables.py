89b80033685625fcc3e12750432ee143b479bed4
abolfazl <abmohajeri1@gmail.com>
2022-01-08 20:22:20 +0330

    Code Refactored

--- variables.py
+++ variables.py
@@ -1,14 +1,14 @@
 import enum
 
-genome1_path = "Coronavirus.fasta"
-genome2_path = "BetaCoronavirus.fasta"
-
 
 class Score(enum.Enum):
     gap = -1
     match = +1
     mismatch = -1
 
+
+genome1_path = "Coronavirus.fasta"
+genome2_path = "BetaCoronavirus.fasta"
 
 codes = {
     'A': '00',
@@ -17,13 +17,13 @@ codes = {
     'T': '11'
 }
 
-minimum_subseq_len = 100    # aka k
+minimum_subseq_len = 100  # aka k
 best_cut_threshold = 20
 base_seq_len = 6
 num_seq_bits = base_seq_len * 2
 middle_fault_tolerance = 50
 
-# plot vars
+# Plot Variables
 COLORS = {
     'A': (0, 0, 255),
     'C': (0, 255, 0),
